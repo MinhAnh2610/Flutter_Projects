fd9bd75e43fae98ad79df39abc5d3ecd3776edb5
Phạm Hoàng Minh Anh <115133340+MinhAnh2610@users.noreply.github.com>
2025-05-05 12:59:49 +0700

    feat: apply theme data for individual widgets

--- expense_tracker/lib/widgets/expenses_list/expense_item.dart
+++ expense_tracker/lib/widgets/expenses_list/expense_item.dart
@@ -10,14 +10,12 @@ class ExpenseItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 16,
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
-            const SizedBox(height: 4,),
+            Text(expense.title, style: Theme.of(context).textTheme.titleLarge),
+            const SizedBox(height: 4),
             Row(
               children: [
                 Text('\$${expense.amount.toStringAsFixed(2)}'),
@@ -25,7 +23,7 @@ class ExpenseItem extends StatelessWidget {
                 Row(
                   children: [
                     Icon(categoryIcons[expense.category]),
-                    const SizedBox(width: 8,),
+                    const SizedBox(width: 8),
                     Text(expense.formattedDate),
                   ],
                 ),
