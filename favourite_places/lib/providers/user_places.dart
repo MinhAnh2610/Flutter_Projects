16377b94a80cc2eceb5254f5bee26ce346ad44ef
Phạm Hoàng Minh Anh <115133340+MinhAnh2610@users.noreply.github.com>
2025-05-09 04:26:51 +0700

    fix: quick fix

--- favourite_places/lib/providers/user_places.dart
+++ favourite_places/lib/providers/user_places.dart
@@ -7,7 +7,11 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
   void addPlace(String title, File image) {
-    final newPlace = Place(title: title, image: image);
+    final newPlace = Place(
+      title: title,
+      image: image,
+      location: PlaceLocation(latitude: 10, longtitude: 10, address: ''),
+    );
     state = [newPlace, ...state];
   }
 }
