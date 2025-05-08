bc73457f699dbe5c423d6b7cda55c2397f220f2f
Phạm Hoàng Minh Anh <115133340+MinhAnh2610@users.noreply.github.com>
2025-05-09 01:56:07 +0700

    feat: add providers

--- favourite_places/lib/main.dart
+++ favourite_places/lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:favourite_places/screens/places.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -23,7 +24,7 @@ final theme = ThemeData().copyWith(
 );
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
--- favourite_places/lib/screens/add_place.dart
+++ favourite_places/lib/screens/add_place.dart
@@ -1,16 +1,29 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:favourite_places/providers/user_places.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AddPlaceScreen extends StatefulWidget {
+class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
 
   @override
-  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
 }
 
-class _AddPlaceScreenState extends State<AddPlaceScreen> {
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+
+  void _savePlace() {
+    final enteredText = _titleController.text;
+    if (enteredText.isEmpty) {
+      return;
+    }
+
+    ref.read(userPlacesProvider.notifier).addPlace(enteredText);
+
+    Navigator.of(context).pop();
+  }
 
   @override
   void dispose() {
@@ -35,7 +48,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
             ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: _savePlace,
               label: const Text('Add Place'),
               icon: const Icon(Icons.add),
             ),
